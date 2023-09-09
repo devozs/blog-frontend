b4f8f5187810bd3278f26f749cbdbb3f8a39c001
devozs <oz.shemesh@datomize.com>
2023-09-09 11:28:23 +0300

    GH action add job dependency

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import Vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [Vue()],
     test: {
-        globals: true
+        globals: true,
+        coverage: {
+            reporter: ['text', 'json-summary', 'json'],
+          }
+      
     }
 })
