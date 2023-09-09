a080a08daa1a8bb7247ba26ab0b4d2afbbe79233
devozs <oz.shemesh@datomize.com>
2023-09-09 11:05:45 +0300

    Adding unit test using Nuxt 3 Vitest

---
+++ tests/imports.test.ts
@@ -0,0 +1,11 @@
+describe('import vue components', () => {
+    test('normal imports as expected', async () => {
+        const cmp = await import ('../components/content/InfoBox.vue')
+        expect(cmp).toBeDefined()
+    })
+    test('dynamic imports as expected', async () => {
+        const name = 'InfoBox'
+        const cmp = await import (`../components/content/${name}.vue`)
+        expect(cmp).toBeDefined()
+    })
+})
